2946e97f51fd99ec9eb003a4a2ae53733d15011d
Nand Morais <nandmorais08@gmail.com>
2026-03-10 17:46:27 -0300

    v2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,6 +4,10 @@ function Footer () {
         <h1 className="text-red-500 bg-red-100 text-center text-3xl p-4 font-bold underline">
         Footer
         </h1>
+        
+                <Link to = "/" className="text-crimson-600 hover:text-pink-600">inicio</Link>
+                <Link to = "/sobre" className="text-crimson-600 hover:text-pink-600">sobre</Link>
+                <Link to = "/sobre" className="text-crimson-600 hover:text-pink-600">contato</Link>
         </>
     )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,14 @@
+import { Link } from "react-router"
+
 function Header () {
     return (
         <>
         <h1 className="text-red-500 bg-red-100 text-center text-3xl p-4 font-bold underline">
         Header
         </h1>
+        <Link to = "/" className="text-crimson-600 hover:text-pink-600">inicio</Link>
+        <Link to = "/sobre" className="text-crimson-600 hover:text-pink-600">sobre</Link>
+        <Link to = "/sobre" className="text-crimson-600 hover:text-pink-600">contato</Link>
         </>
     )
 }
